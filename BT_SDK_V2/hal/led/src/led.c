0a7d368f28a1470956ceb9b5f3a8ac32e1b20095
zhouxiao619-art <zhouxiao619@gmail.com>
2026-03-20 16:44:18 +0800

    修复乱码注释并适配16MHz晶振

--- BT_SDK_V2/hal/led/src/led.c
+++ BT_SDK_V2/hal/led/src/led.c
@@ -3,17 +3,17 @@
 #include "app.h"
 #include "timer.h"
 
-uint16_t led_on_time[LED_NUM]; 				//LED亮时间
-uint16_t led_off_time[LED_NUM];				//LED灭时间
+uint16_t led_on_time[LED_NUM]; 				//LED点亮时间
+uint16_t led_off_time[LED_NUM];				//LED熄灭时间
 uint8_t led_twinkle_num[LED_NUM]; 			//闪烁次数
 uint8_t led_constant_flash_flag[LED_NUM];	//持续闪烁标志位
 uint16_t led_tick[LED_NUM];    				//led计时
 uint8_t led_flag[LED_NUM];    				//led状态切换标志位
-uint16_t led_work_enable[LED_NUM];			//不同led使能位
-uint16_t led_enable_flay[LED_NUM];			//不同led状态使能位
+uint16_t led_work_enable[LED_NUM];			//控制led使能位
+uint16_t led_enable_flay[LED_NUM];			//控制led状态标志位
 
-ktimer_t led_timer;							//led定时器，用于超时处理
-uint8_t led_pro_timeout = 0x00;				//led超时处理标志位
+ktimer_t led_timer; 							//led超时处理定时器
+uint8_t led_pro_timeout = 0x00; 				//led超时标志位
 
 void set_led_work_enable(uint8_t num, uint8_t en)
 {
@@ -64,14 +64,14 @@ uint16_t led_work_busy(void)
 void reset_led_all_flay(void)
 {
 	for(uint8_t i=0; i<LED_NUM; i++){
-		led_on_time[i] 				= 0x00; 	//LED亮时间
-		led_off_time[i] 			= 0x00; 	//LED灭时间
-		led_twinkle_num[i] 			= 0x00; 	//闪烁次数
+		led_on_time[i] 			= 0x00; 	//LED点亮时间
+		led_off_time[i] 		= 0x00; 	//LED熄灭时间
+		led_twinkle_num[i] 		= 0x00; 	//闪烁次数
 		led_constant_flash_flag[i] 	= 0x00; 	//持续闪烁标志位
-		led_tick[i] 				= 0x00; 	//led计时
-		led_flag[i] 				= 0x00; 	//led状态切换标志位
-		led_work_enable[i] 			= 0x00; 	//不同led使能位
-		led_enable_flay[i] 			= 0x00; 	//不同led状态使能位
+		led_tick[i] 			= 0x00; 	//led计时
+		led_flag[i] 			= 0x00; 	//led状态切换标志位
+		led_work_enable[i] 		= 0x00; 	//控制led使能位
+		led_enable_flay[i] 		= 0x00; 	//控制led状态标志位
 	}
 }
 
@@ -95,7 +95,7 @@ void led_timer_strat(void)
 }
 
 /**
- * @brief LED?????
+ * @brief LED初始化
  * @param void
  */
 void led_init(void)
@@ -130,7 +130,7 @@ void led_1_2_charge_charging(void)
 	timer_en(TIM4, DISABLE);
 }
 
-//此接口用于设置LED状态
+//设置LED亮度等级
 void set_led_level(uint8_t level)
 {
 	//控制LED1_2
@@ -147,7 +147,7 @@ void set_led_level(uint8_t level)
 		}
 	}
 	
-//	if(led_work_enable[LED_2]==ENABLE){ 		//控制LED3_4
+//	if(led_work_enable[LED_2]==ENABLE){ 		//????LED3_4
 //		set_led_work_enable(LED_2, DISABLE);
 //		led_3_6_reset();
 //		if(level){
@@ -163,7 +163,7 @@ void set_led_level(uint8_t level)
 //		}else{
 //			DEMO_PRINTF("set_led_level led2 low\n");
 //			reset_led_enable_flay(LED_2);
-//			if(led_enable_flay[LED_3]==0x01){ 		//LED5_6原来处于开启状态需要关闭
+//			if(led_enable_flay[LED_3]==0x01){ 		//LED5_6??????????????????
 //				gpio_remapmode_set(LED3_6_IO, GPIO_PINMUX_GPIO);
 //				gpio_pinmode_set(LED3_6_IO, GPIO_INOUT_MODE_OUTPUT);
 //				gpio_reset_pin(LED3_6_IO);
@@ -171,7 +171,7 @@ void set_led_level(uint8_t level)
 //		}
 //	}
 //	
-//	if(led_work_enable[LED_3]==ENABLE){ 		//控制LED5_6
+//	if(led_work_enable[LED_3]==ENABLE){ 		//????LED5_6
 //		set_led_work_enable(LED_3, DISABLE);
 //		led_3_6_reset();
 //		if(level){
@@ -187,7 +187,7 @@ void set_led_level(uint8_t level)
 //		}else{
 //			DEMO_PRINTF("set_led_level led3 low\n");
 //			reset_led_enable_flay(LED_3);
-//			if(led_enable_flay[LED_2]==0x01){ 	//LED3_4原来处于开启状态需要开启
+//			if(led_enable_flay[LED_2]==0x01){ 	//LED3_4???????????????????
 //				gpio_remapmode_set(LED3_6_IO, GPIO_PINMUX_GPIO);
 //				gpio_pinmode_set(LED3_6_IO, GPIO_INOUT_MODE_OUTPUT);
 //				gpio_set_pin(LED3_6_IO);
@@ -196,10 +196,10 @@ void set_led_level(uint8_t level)
 //	}
 }
 
-//led模式处理函数
+//led模式处理
 void led_mode_pro(void)
 {
-//	//LED3_4和LED5_6模式需要单独处理
+//	//LED3_4和LED5_6共用IO处理
 //	if((led_work_enable[LED_2] == 0x00) && (led_work_enable[LED_3] == 0x00)){
 //		if((led_enable_flay[LED_2]==0x01) && (led_enable_flay[LED_3]==0x01)){
 //				led_3_6_set();
@@ -219,7 +219,7 @@ void led_mode_pro(void)
 //		}
 //	}
 	
-	//LED1_2模式需要单独处理
+	//LED1_2单独IO处理
 	if(led_work_enable[LED_1] == 0x00){
 		if(led_enable_flay[LED_1]==0x01){
 			gpio_set_pin(LED1_2_IO);
@@ -229,7 +229,7 @@ void led_mode_pro(void)
 	}
 }
 
-//需要闪烁的LED、占空比和闪烁次数使用此接口
+//设置LED闪烁参数
 void set_led_parma(uint32_t led_time,uint16_t led_num, uint8_t led, uint8_t timeout_en)
 {
 	lib_timer_stop(&led_timer);
@@ -246,7 +246,7 @@ void set_led_parma(uint32_t led_time,uint16_t led_num, uint8_t led, uint8_t time
 	led_twinkle_num[led]			= (led_num >> 8) & 0xFF;
 	led_constant_flash_flag[led] 	= led_num & 0xFF;
 	
-	led_flag[led] 				= led_enable_flay[led];		//获取led当前状态
+	led_flag[led] 			= led_enable_flay[led];		//保存led当前状态
 	led_tick[led]					= 0;
 	
 	if(timeout_en){
@@ -259,14 +259,14 @@ void set_led_parma(uint32_t led_time,uint16_t led_num, uint8_t led, uint8_t time
 
 void user_led_set(uint8_t led_num, uint8_t en)
 {
-	//使用LED工作位
+	//设置LED使能位
 	set_led_work_enable(led_num, ENABLE);
-	//设置LED电平
+	//设置LED状态
 	set_led_level(en);
 }
 
 /**
- * @brief LED闪烁功能处理函数
+ * @brief LED闪烁处理
  * @param void
  */
 void led_fun(void)
@@ -276,7 +276,7 @@ void led_fun(void)
 	}
 	uint8_t ret = 0;
 	
-	//先判断LED的状态是否需要改变
+	//???ж?LED?????????????
 	for(uint8_t i=0; i<LED_NUM; i++){
 		if((led_twinkle_num[i] > 0)||(led_constant_flash_flag[i]))
 		{
@@ -294,7 +294,7 @@ void led_fun(void)
 					
 					led_tick[i] = 0;
 					led_flag[i] = (!led_flag[i]);
-					//执行LED状态切换
+					//???LED???л?
 					if(led_flag[i] == LIGHT_STATE_ON){
 						DEMO_PRINTF("set_led_enable_flay[%d]\n",i);
 						set_led_enable_flay(i); 		
